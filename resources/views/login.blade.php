f95492c1aac6a1630ec42110acb636d16894b33b
jamesjosol <james.josol70@gmail.com>
2021-09-22 12:27:28 +0800

    update 1

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,18 +8,18 @@
     <div class="col-md-4 offset-md-4">
         <div class="card">
             <div class="card-header bg-primary text-white">
-                <h3 class="card-title"><i class="fa fa-user"></i> User Login</h3>
+                <h3 class="card-title"><i class="fas fa-lock"></i> User Login</h3>
             </div>
             <div class="card-body">
                 <form action="{{ url('/login') }}" method="POST">
                     {{csrf_field()}}
                     <div class="form-group @error('email') has-error @enderror">
-                        <label for="email">Email</label>
+                        <label for="email"><i class="fas fa-at"></i> Email</label>
                         <input type="email" name="email" value="{{ old('email') }}" id="email" class="form-control">
                         <span class="errspan" id="errspan">{{ $errors->first('email') }}</span>
                     </div>
                     <div class="form-group @error('password') has-error @enderror">
-                        <label for="password">Password</label>
+                        <label for="password"><i class="fas fa-key"></i> Password</label>
                         <input type="password" name="password" id="password" class="form-control">
                         <span class="errspan" id="errspan">{{ $errors->first('password') }}</span>
                     </div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -12,23 +12,23 @@
                 <form class="form" action="{{ url('/register') }}" method="POST">
                     {{csrf_field()}}
                     <div class="form-group @error('name') has-error @enderror">
-                        <label for="name">Name</label>
+                        <label for="name"><i class="far fa-user"></i> Name</label>
                         <input type="text" name="name" id="name" value="{{ old('name') }}" class="form-control">
                         <span class="errspan" id="errspan">{{ $errors->first('name') }}</span>
                     </div>
                     <div class="form-group @error('email') has-error @enderror">
-                        <label for="email">Email</label>
+                        <label for="email"><i class="fas fa-at"></i> Email</label>
                         <input type="email" name="email" id="email" value="{{ old('email') }}" class="form-control">
                         <span class="errspan" id="errspan">{{ $errors->first('email') }}</span>
                     </div>
                     <div class="form-group login-form @error('contactNo') has-error @enderror">
-                        <label for="contactNo">Contact No.</label>
+                        <label for="contactNo"><i class="far fa-phone-alt"></i> Contact No.</label>
                         <span class="prepend">+63</span>
                         <input type="text" name="contactNo" id="contactNo" value="{{ old('contactNo') ? old('contactNo') : '9458090921' }}" class="form-control">
                         <span class="errspan" id="errspan">{{ $errors->first('contactNo') }}</span>
                     </div>
                     <div class="form-group @error('password') has-error @enderror">
-                        <label for="password">Password</label>
+                        <label for="password"><i class="fas fa-key"></i> Password</label>
                         <input type="password" name="password" id="password" class="form-control">
                         <span class="errspan" id="errspan">{{ $errors->first('password') }}</span>
                     </div>
